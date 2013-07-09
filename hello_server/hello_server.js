787c064b66c63c29f3881d7904189e1503538631
Shahzeb Ihsan <shahzeb@gmail.com>
2013-07-09 14:22:49 +0800

    Added "hello_server" example which listens for HTTP connections and TCP connections on the specified ports and responds with "Hello World" and then closes the connection.

---
+++ hello_server/hello_server.js
@@ -0,0 +1,24 @@
+var http = require('http')
+var util = require('util');
+var net = require('net');
+
+var tcp_server = net.createServer(function (socket) {
+  util.puts("Connection from " + socket.remoteAddress);
+  socket.write("Hello World\n");
+  socket.end();
+
+  socket.on('end', function () {
+  });
+});
+
+var http_server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+http_server.listen(8000);
+tcp_server.listen(7000);
+
+console.log("HTTP Server running at http://127.0.0.1:8000/");
+console.log("TCP Server running at 127.0.0.1:7000");
+
