70f9b35b8615d2ade8ed3e75dbe3522377e6b053
Shahzeb Ihsan <shahzeb@gmail.com>
2013-07-28 04:57:17 +0800

    Code cleanup

--- chat_server/secure_chat_server.js
+++ chat_server/secure_chat_server.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-// Note to my idiot self: Cookies are used for http/https state saving, Socket.io session authorization is for websocket connection authentication.
-
 /* Setup dependencies */
 var jade = require('jade');
 var io = require('socket.io');
@@ -23,45 +21,52 @@ var options = {
   ca: fs.readFileSync('cert/ca.crt')
 };
 
-/* Port to listen on */
-var port = 17231;
-
 /* Application version */
 var version = '0.1'
 
 /* Initialize ExpressJS "app" */
 var app = express(express.logger()); 
 
-//==============================================================================
-app.use(express.cookieParser());
-app.use(express.session({secret: 'secret', key: 'express.sid'}));
-app.use(function (req, res, next) {
-  // check if client sent cookie
-  var cookie = req.cookies.cookieName;
-  console.log(clc.red("cookie: " + cookie + ", url: " + req.url));
-  if (cookie === undefined)
-  {
-    res.cookie('cookieName', 123456789, { maxAge: 900000, httpOnly: false });
-    console.log(clc.red('Cookie created successfully'));
-    authenticated = false;
-  } 
-  else
-  {
-    // yes, cookie was already present 
-    console.log(clc.red('Cookie exists: ' + cookie));
-    authenticated = true;
-  } 
-  next(); // <-- important!
+app.configure(function () {
+  /* Use Cookie middle ware */
+  app.use(express.cookieParser('test'));
+
+  /* Use session middle ware */
+  app.use(express.session({secret: 'secret', key: 'express.sid'}));
+  
+  /* Custom middleware to check for and set cookies */
+  app.use(function (req, res, next) {
+    /* Check if the client has sent a cookie */
+    var cookie = req.signedCookies.cookieName;
+    console.log(clc.yellow('   debug - Cookie: ' + cookie + ", URL: " + req.url));
+
+    if (cookie === undefined) {
+      res.cookie('cookieName', 123456789, {maxAge: 900000, httpOnly: false, signed: true});
+      console.log(clc.yellow('   debug - Cookie created successfully'));
+      authenticated = false;
+    } else {
+      /* The cookie is already present */ 
+      console.log(clc.yellow('   debug - Cookie exists: ' + cookie));
+      authenticated = true;
+    } 
+    
+    /* Invoke the next middleware */
+    next();
+  });
+  
+  /* Use the Static middleware to specify that all static content is
+     to be served from the "./public" directory */
+  app.use(express.static(__dirname + '/public'));
 });
-//==============================================================================
 
 /* Specify directories */
-app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/tpl');
 
-/* Specify that the template engine we are using is Jade */
+
+/* Specify that the template engine we are using is Jade and set
+   the template directory */
 app.set('view engine', 'jade');
 app.engine('jade', jade.__express);
+app.set('views', __dirname + '/tpl');
 
 /* Render view on default GET */
 app.get('/', function(req, res){
@@ -72,37 +77,42 @@ app.get('/', function(req, res){
     }
 });
 
+/* Render the chat page if everything is in order */
 app.get('/success', function(req, res){
     res.render('secure_page');
 });
 
 /* Create HTTP server  */
 var http_server = http.createServer(app).listen(80, function() {
-    console.log('HTTP server running...');
+    console.log(clc.yellow('   debug - HTTP server running...'));
 });
 
 /* Create HTTPS server */
 var https_server = https.createServer(options, app).listen(443, function() {
-    console.log('HTTPS server running...');
+    console.log(clc.yellow('   debug - HTTPS server running...'));
 });
 
+/* Create the socket.io server */
 var server = io.listen(https_server);
-console.log('Listening on port ' + port); 
+console.log(clc.yellow('   debug - socket.io server running')); 
 
-/* Chat message handling */
+/* Connection handling */
 server.sockets.on('connection', function (socket) {
+    /* Print client particulars */
     var address = socket.handshake.address.address;
     var port = socket.handshake.address.port;
-    console.log(clc.blue('   debug - New connection from ' + address + ':' + port));
+    console.log(clc.yellow('   debug - New connection from ' + address + ':' + port));
 
+    /* On 'send' event from client */
     socket.on('send', function (data) {
         data.message = address + ':' + port + '> ' + data.message;
         server.sockets.emit('message', data);
     });
 
+    /* On 'login' evemt from client */
     socket.on('login', function (data) {
-        console.log(clc.blue('   debug - Data <<' + data.username + ',' + data.password + '>>  from ' + socket.handshake.address.address  + ':' +  socket.handshake.address.port));
-        console.log(clc.blue('   debug - Authenticated: ' + authenticated));
+        console.log(clc.yellow('   debug - Data <<' + data.username + ',' + data.password + '>>  from ' + socket.handshake.address.address  + ':' +  socket.handshake.address.port));
+        console.log(clc.yellow('   debug - Authenticated: ' + authenticated));
         socket.emit('redirect', { toUrl: 'success'});
     });
 });
